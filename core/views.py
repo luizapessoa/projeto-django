18aac10528c903ee55ebda2aca3493444f7addc0
MARIA LUIZA PESSOA SILVEIRA <MARIA2425@IOT>
2024-11-21 14:35:50 -0300

    mensagem

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pessoa
 
 # Create your views here.
@@ -6,3 +6,25 @@ from .models import Pessoa
 def home(request): 
     pessoas = Pessoa.objects.all()
     return render(request, "index.html", {"pessoas": pessoas})
+
+def salvar(request):
+    varnome = request.POST.get("nome")
+    Pessoa.objects.create(nome=varnome)
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {"pessoas": pessoas})
+
+def editar(request, id): 
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request, "update.html", {"pessoa": pessoa})
+
+def update(request, id):
+    novoNome = request.POST.get("nome")
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.nome = novoNome
+    pessoa.save()
+    return redirect(home)
+
+def delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect (home)
